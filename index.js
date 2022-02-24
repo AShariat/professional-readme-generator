518461d7c86417d8c36220d113ea9d6966c0c23a
Amir Shariat <amirshariat1987@gmail.com>
2022-02-23 17:14:51 -0700

    promptQuestion function

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+const promptQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
+    }
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptQuestions();
+}
 
 // Function call to initialize app
 init();
